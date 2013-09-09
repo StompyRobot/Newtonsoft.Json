a74f01123463ec349d3a0993126c63f8fdd92c0f
Simon Moles <simon.moles@gmail.com>
2013-09-09 22:07:08 +0100

    Fixed deserializing dynamic type ($type) collections on Unity Win8 C++

--- Src/Newtonsoft.Json/Serialization/DefaultSerializationBinder.cs
+++ Src/Newtonsoft.Json/Serialization/DefaultSerializationBinder.cs
@@ -49,13 +49,13 @@ namespace Newtonsoft.Json.Serialization
       {
         Assembly assembly;
 
-#if !(SILVERLIGHT || NETFX_CORE || PORTABLE40 || PORTABLE)
+#if !(SILVERLIGHT || NETFX_CORE || PORTABLE40 || PORTABLE || UNITY)
         // look, I don't like using obsolete methods as much as you do but this is the only way
         // Assembly.Load won't check the GAC for a partial name
 #pragma warning disable 618,612
         assembly = Assembly.LoadWithPartialName(assemblyName);
 #pragma warning restore 618,612
-#elif NETFX_CORE || PORTABLE
+#elif NETFX_CORE || PORTABLE || UNITY
         assembly = Assembly.Load(new AssemblyName(assemblyName));
 #else
         assembly = Assembly.Load(assemblyName);
